95bfb2222d2395b78992f8c677ef9a0201307f4a
SebastianGeroli <sebastiangeroli@outlook.com>
2021-01-30 14:05:14 -0300

    ball enemy mejora

--- Assets/Scripts/BallEnemy.cs
+++ Assets/Scripts/BallEnemy.cs
@@ -13,9 +13,11 @@ public partial class BallEnemy : MonoBehaviour
     [SerializeField]
     float maxAttackDistance = 3f;
     [SerializeField]
-    float reachMaxDistanceIn = 2f;
+    float movementSpeed = 2f;
+    [SerializeField]
     bool gameOver = false;
     Vector2 translation;
+    bool isInRange = false;
 
     Rigidbody2D rigidbody2D;
     void Start()
@@ -30,41 +32,45 @@ public partial class BallEnemy : MonoBehaviour
         {
             return;
         }
-        translation = Vector2.Lerp(transform.position, Utility.PlayerPos.position, Time.deltaTime / reachMaxDistanceIn);
+        translation = Vector2.Lerp(transform.position, Utility.PlayerPos.position, Time.deltaTime * movementSpeed);
         transform.Translate(translation - (Vector2)transform.position);
 
     }
+    private void ChangeAttack(bool attack)
+    {
+        this.isInRange = attack;
+    }
     private void MoveTowardsOrigin()
     {
-        translation = Vector2.Lerp(transform.position, anchorPosition.position, Time.deltaTime / reachMaxDistanceIn);
+        translation = Vector2.Lerp(transform.position, anchorPosition.position, Time.deltaTime * movementSpeed / 2);
         transform.position = translation;
     }
     IEnumerator Attack()
     {
         float timer = 0;
-        bool attack = true;
+        float timer2 = 0;
         while (gameOver == false)
         {
-            if (attack)
+            if (isInRange)
             {
                 timer += Time.deltaTime;
                 MoveTowardPlayer();
-                if (timer >= reachMaxDistanceIn)
+                if (timer >= movementSpeed)
                 {
-                    timer = 0;
-                    attack = false;
+                    MoveTowardsOrigin();
+                    timer2 += Time.deltaTime;
+                    if (timer2 >= movementSpeed / 2)
+                    {
+                        timer = 0;
+                        timer2 = 0;
+                    }
                 }
             }
             else
             {
-                Debug.Log("Moving To origin");
-                timer += Time.deltaTime;
+                timer = 0;
+                timer2 = 0;
                 MoveTowardsOrigin();
-                if (timer >= reachMaxDistanceIn / 2)
-                {
-                    timer = 0;
-                    attack = true;
-                }
             }
 
             yield return new WaitForEndOfFrame();
---
+++ Assets/Scripts/EnemyAttackController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class EnemyAttackController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            BroadcastMessage("ChangeAttack", true, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            BroadcastMessage("ChangeAttack", false, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
