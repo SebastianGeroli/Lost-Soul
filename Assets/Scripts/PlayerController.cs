6d0907debbf9023a32de975862d2a8c7af483a47
SebastianGeroli <sebastiangeroli@outlook.com>
2021-01-30 09:29:29 -0300

    player can move
    player can move left to right and jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,24 +7,28 @@ public class PlayerController : MonoBehaviour
 {
 
 
-    private bool jump;
-    private bool isJumping = false;
+    private bool jump = false;
+    [SerializeField]
+    private bool isGrounded = true;
+
 
     Rigidbody2D rigidbody2D;
+    [Header("Movement Control")]
+    [SerializeField]
+    private float movementSpeed = 3f;
+    [SerializeField]
+    private float controlInAir = 0.7f;
+    [SerializeField]
+    private float controlGrounded = 1f;
     [SerializeField]
     private float jumpForce = 5f;
 
+    private Vector2 newVelocity;
+    #region  Unity Functions
     private void Awake()
     {
         rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Jump"))
@@ -39,32 +43,51 @@ public class PlayerController : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        if (jump && isJumping == false)
+        if (jump && isGrounded == true)
         {
             Jump();
         }
+        Move(Input.GetAxis("Horizontal"));
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.layer == 9)
         {
-            ResetIsJumping();
+            ResetGrounded();
         }
     }
+    #endregion //Unity Functions
 
+
+    #region Private Functions 
     private void Jump()
     {
-        if (isJumping)
+        if (isGrounded == false)
         {
             return;
         }
-        isJumping = true;
+        isGrounded = false;
         rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
     }
 
 
-    private void ResetIsJumping()
+    private void ResetGrounded()
     {
-        isJumping = false;
+        isGrounded = true;
     }
+
+    private void Move(float movementInX)
+    {
+        newVelocity = rigidbody2D.velocity;
+        if (isGrounded)
+        {
+            newVelocity.x = movementInX * controlGrounded * movementSpeed;
+            rigidbody2D.velocity = newVelocity;
+        }
+        else {
+            newVelocity.x = movementInX * controlInAir * movementSpeed;
+            rigidbody2D.velocity = newVelocity;
+        }
+    }
+    #endregion //Private Functions
 }
